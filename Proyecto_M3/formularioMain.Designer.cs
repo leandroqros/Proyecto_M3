9d29b1a3a110e9a15b38e09a62f0b7c7679a59eb
Leandro <leandroqros@gmail.com>
2022-05-23 16:55:43 +0200

    Añadido botones y campos

---
+++ Proyecto_M3/formularioMain.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Proyecto_M3
+{
+    partial class formularioMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btArchivo = new System.Windows.Forms.Button();
+            this.btLoad = new System.Windows.Forms.Button();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.cbPadre = new System.Windows.Forms.ComboBox();
+            this.cbHijo = new System.Windows.Forms.ComboBox();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.lbArchivo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.rtbDocSalida = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btArchivo
+            // 
+            this.btArchivo.Location = new System.Drawing.Point(322, 75);
+            this.btArchivo.Name = "btArchivo";
+            this.btArchivo.Size = new System.Drawing.Size(75, 23);
+            this.btArchivo.TabIndex = 0;
+            this.btArchivo.Text = "...";
+            this.btArchivo.UseVisualStyleBackColor = true;
+            // 
+            // btLoad
+            // 
+            this.btLoad.Location = new System.Drawing.Point(403, 75);
+            this.btLoad.Name = "btLoad";
+            this.btLoad.Size = new System.Drawing.Size(75, 23);
+            this.btLoad.TabIndex = 1;
+            this.btLoad.Text = "Cargar";
+            this.btLoad.UseVisualStyleBackColor = true;
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Location = new System.Drawing.Point(322, 104);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(156, 23);
+            this.btBuscar.TabIndex = 2;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Location = new System.Drawing.Point(322, 133);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(156, 23);
+            this.btAgregar.TabIndex = 3;
+            this.btAgregar.Text = "Agergar";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            // 
+            // cbPadre
+            // 
+            this.cbPadre.FormattingEnabled = true;
+            this.cbPadre.Location = new System.Drawing.Point(47, 104);
+            this.cbPadre.Name = "cbPadre";
+            this.cbPadre.Size = new System.Drawing.Size(121, 21);
+            this.cbPadre.TabIndex = 4;
+            // 
+            // cbHijo
+            // 
+            this.cbHijo.FormattingEnabled = true;
+            this.cbHijo.Location = new System.Drawing.Point(182, 106);
+            this.cbHijo.Name = "cbHijo";
+            this.cbHijo.Size = new System.Drawing.Size(121, 21);
+            this.cbHijo.TabIndex = 5;
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Location = new System.Drawing.Point(27, 24);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(93, 13);
+            this.lbTitulo.TabIndex = 6;
+            this.lbTitulo.Text = "Solidarity At Home";
+            // 
+            // lbArchivo
+            // 
+            this.lbArchivo.AutoSize = true;
+            this.lbArchivo.Location = new System.Drawing.Point(44, 80);
+            this.lbArchivo.Name = "lbArchivo";
+            this.lbArchivo.Size = new System.Drawing.Size(46, 13);
+            this.lbArchivo.TabIndex = 7;
+            this.lbArchivo.Text = "Archivo:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // rtbDocSalida
+            // 
+            this.rtbDocSalida.Location = new System.Drawing.Point(47, 180);
+            this.rtbDocSalida.Name = "rtbDocSalida";
+            this.rtbDocSalida.Size = new System.Drawing.Size(431, 131);
+            this.rtbDocSalida.TabIndex = 9;
+            this.rtbDocSalida.Text = "";
+            // 
+            // formularioMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbDocSalida);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbArchivo);
+            this.Controls.Add(this.lbTitulo);
+            this.Controls.Add(this.cbHijo);
+            this.Controls.Add(this.cbPadre);
+            this.Controls.Add(this.btAgregar);
+            this.Controls.Add(this.btBuscar);
+            this.Controls.Add(this.btLoad);
+            this.Controls.Add(this.btArchivo);
+            this.Name = "formularioMain";
+            this.Text = "Solidarity At Home - Subtropical";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btArchivo;
+        private System.Windows.Forms.Button btLoad;
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.ComboBox cbPadre;
+        private System.Windows.Forms.ComboBox cbHijo;
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.Label lbArchivo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox rtbDocSalida;
+    }
+}
+
